8b64baa373793f25e53b32ed479a87414672a8c3
T1moha5 <timoha358@gmail.com>
2022-02-07 14:52:09 +0300

    создание задачи на каждом потоке ЦП

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-function changeColor(newColor){
-    var element = document.getElementById("text");
-    element.style.color = newColor;
+const os = require('os');
+const cluster = require('cluster');
+
+if(cluster.isMaster){
+    for(let i = 0; i < os.cpus().length; i++) {
+        cluster.fork()
+    }
+}else{
+    console.log('процесс с pid= ', process.pid, 'запущен')
+    setInterval(() => {
+
+    }, 5000)
 }
+
+
